ad5e10e15b50e0751134514dec9a039de530590d
pascbjumper2 <stowers.joshua@live.com>
2015-01-22 16:22:59 -0600

    wow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  @designs = Design.first
+  # Commented to allow migrations. Dumb rake.
+  # @designs = Design.first
 
 end
